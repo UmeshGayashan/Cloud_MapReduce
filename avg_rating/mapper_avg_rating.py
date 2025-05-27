72fd225fe5b69cc9c7b3b359bcbbace8de7491ef
H.L.D.U.G Perera <138327662+UmeshGayashan@users.noreply.github.com>
2025-05-27 18:14:07 +0530

    to fix some naming issues as preprocessing part

--- avg_rating/mapper_avg_rating.py
+++ avg_rating/mapper_avg_rating.py
@@ -5,7 +5,7 @@ import csv
 reader = csv.reader(sys.stdin)
 for row in reader:
     try:
-        brand = row[1].strip()
+        brand = row[1].strip().lower().replace(" ", "")
         rating = float(row[3])
         if brand:
             print(f"{brand}\t{rating}")
